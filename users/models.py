2f008998b6a3576cbd4b0d968a2f44dda20b5e5b
BrunaCardoso7 <obitadrawing@gmail.com>
2024-11-09 00:33:21 -0300

    fix: corrigi validação de username e password como únicos

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=12, null=False, blank=False)
+    username = models.CharField(max_length=12, null=False, blank=False, unique=True)
     email = models.EmailField(max_length=150, null=False, blank=False, unique=True)
-    password = models.CharField(max_length=32, null=False, blank=False)
+    password = models.CharField(max_length=32, null=False, blank=False, unique=True)
     created_at = models.DateTimeField(null=False, blank=False)
